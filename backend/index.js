28d9f1c436a3caff30b046f864e193bd7f86a3ff
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-10-15 02:23:33 +0300

    backend<>adding nodemon package

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+//API
+app.get("/user", () => {
+    console.log("hello world")
+})
+
+//server
 app.listen(8000, (err) => {
     if (err) console.log(err)
     console.log("server started on port 8000")
