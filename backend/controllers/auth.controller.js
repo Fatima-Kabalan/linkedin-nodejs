1f99c216bc2a602cc7a11af4325f679e6a2c0a41
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-10-16 18:46:40 +0300

    backend<>adding signup api

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -21,6 +21,25 @@ const login = async (req,res)=>{
 }
 
 
+const signup = async (req, res)=>{
+    const {name, email, password} = req.body;
+    try{
+        const user = new User();
+        user.name = name;
+        user.email = email;
+        user.password = await bcrypt.hash(password, 10);
+
+        await user.save();
+        res.json(user)
+    }catch(err){
+        res.status(400).json({
+            message: err.message
+        })
+    }
+}
+
+
 module.exports = {
     login,
+    signup
 }
