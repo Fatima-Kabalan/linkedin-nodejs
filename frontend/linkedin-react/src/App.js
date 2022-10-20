788535091689f77156f657ff23d4f4c2dea8aac7
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-10-21 01:21:21 +0300

    frontend<>styling the signin signup page

--- frontend/linkedin-react/src/App.js
+++ frontend/linkedin-react/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import LandingPage from './pages/LandingPage';
 import LogIn from './pages/LogIn';
+import "./App.css";
 
 function App() {
   return (
--- frontend/linkedin-react/src/pages/LogIn.js
+++ frontend/linkedin-react/src/pages/LogIn.js
@@ -3,19 +3,38 @@ import React from 'react';
 const LogInPage = () =>{
     return(
         <>
-        <form>
-            <div className='flex-row'>
-                <h1>Sign in</h1>
-                <img src="./../public/logo.png"></img>
+        {/* <div className='container'>
+            <div className='flex'>
+                <form className=''>
+                    <div className='flex-row flex'>
+                        <div className="img-logo">
+                            <img  src="https://th.bing.com/th/id/R.6e154f80072e0f134105ec35599d74a6?rik=rPaegHNYFDueCw&pid=ImgRaw&r=0" alt="logo"></img>
+                        </div>
+                        <h1 className='sign-in'>Sign in</h1>
+                    </div>
+                    <div className=''>
+                        <h6>Stay updated on your professional world</h6>
+                        <div className='flex-column'>
+                            <input type="email" placeholder='Email'></input>
+                            <input type="password" placeholder='Password'></input>
+                        </div>
+                        <button className='btn-blue'>Sign in</button>
+                        <p>New to LinkedIn?<span><a href="signup" />Join now</span></p>
+                    </div>
+                </form>
             </div>
-            <h6>Stay updated on your professional world</h6>
-            <div className='flex-column'>
-                <input type="email" placeholder='Email'></input>
-                <input type="password" placeholder='Password'></input>
-                <button>Sign in</button>
-            </div>
-            <p>New to LinkedIn?<span><a href="signup" />Join now</span></p>
-        </form>
+        </div>
+        */}
+        <div className="signin-box">
+            <h1>Sign in</h1>
+            <h4>Stay updated on your professional world</h4>
+            <form>
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <input type="button" value="Submit" />
+                <p className="para-2">New to LinkedIn ? <a href="signup">Join now</a></p>
+            </form>
+        </div>
         </>
         )
 }
