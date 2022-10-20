5c42ae4ec8abecd5cb6dc6c7e9fb50e24a4b9d54
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-10-21 01:46:12 +0300

    frontend<>restyling the login page

--- frontend/linkedin-react/src/pages/LandingPage.js
+++ frontend/linkedin-react/src/pages/LandingPage.js
@@ -1,5 +1,17 @@
-// import React from 'react';
+import React from 'react';
 
-// const LandingPage = () => {
-//     return ();
-// }
+const LandingPage = () => {
+    return (
+        <>
+        <nav>
+            <img src="https://th.bing.com/th/id/R.6e154f80072e0f134105ec35599d74a6?rik=rPaegHNYFDueCw&pid=ImgRaw&r=0" alt="logo"></img>
+            <ul>
+                <li><a href="" >Home</a></li>
+                <li><a href="" >Jobs</a></li>
+                <li><a href="" >Notifications</a></li>
+                <li><a href="" >Me</a></li>
+            </ul>
+        </nav>
+        </>
+    );
+}
