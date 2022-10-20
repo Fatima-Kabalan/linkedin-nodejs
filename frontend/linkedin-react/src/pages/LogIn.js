b53e8851727bc9824d07415a0aa23d0aee96b34f
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-10-21 01:27:14 +0300

    frontend<>deleting the unnecessary comments in the signin signup page

--- frontend/linkedin-react/src/pages/LogIn.js
+++ frontend/linkedin-react/src/pages/LogIn.js
@@ -25,6 +25,9 @@ const LogInPage = () =>{
             </div>
         </div>
         */}
+        <div className="img-logo">        
+            <img src="https://th.bing.com/th/id/R.6e154f80072e0f134105ec35599d74a6?rik=rPaegHNYFDueCw&pid=ImgRaw&r=0" alt="logo"></img>
+        </div>
         <div className="signin-box">
             <h1>Sign in</h1>
             <h4>Stay updated on your professional world</h4>
