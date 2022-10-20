9e0d25590392a3d5721be40185ae1fc0891b70f1
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-10-21 01:28:12 +0300

    frontend<>deleting the unnecessary comments in the loginpage

--- frontend/linkedin-react/src/pages/LogIn.js
+++ frontend/linkedin-react/src/pages/LogIn.js
@@ -3,28 +3,6 @@ import React from 'react';
 const LogInPage = () =>{
     return(
         <>
-        {/* <div className='container'>
-            <div className='flex'>
-                <form className=''>
-                    <div className='flex-row flex'>
-                        <div className="img-logo">
-                            <img  src="https://th.bing.com/th/id/R.6e154f80072e0f134105ec35599d74a6?rik=rPaegHNYFDueCw&pid=ImgRaw&r=0" alt="logo"></img>
-                        </div>
-                        <h1 className='sign-in'>Sign in</h1>
-                    </div>
-                    <div className=''>
-                        <h6>Stay updated on your professional world</h6>
-                        <div className='flex-column'>
-                            <input type="email" placeholder='Email'></input>
-                            <input type="password" placeholder='Password'></input>
-                        </div>
-                        <button className='btn-blue'>Sign in</button>
-                        <p>New to LinkedIn?<span><a href="signup" />Join now</span></p>
-                    </div>
-                </form>
-            </div>
-        </div>
-        */}
         <div className="img-logo">        
             <img src="https://th.bing.com/th/id/R.6e154f80072e0f134105ec35599d74a6?rik=rPaegHNYFDueCw&pid=ImgRaw&r=0" alt="logo"></img>
         </div>
