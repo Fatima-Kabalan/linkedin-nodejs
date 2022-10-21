60ade7f81b61b097ecbd7018a97a9631d963a943
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-10-21 12:54:40 +0300

    frontend<>styling the navbar

--- frontend/linkedin-react/src/pages/LandingPage.js
+++ frontend/linkedin-react/src/pages/LandingPage.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import {FiBell,FiUser,FiHome} from "react-icons/fi";
-import { FcBriefcase } from "react-icons/fc";
+
 
 
 const LandingPage = () => {
     return (
         <>
-        <nav>
+        <nav className='flex'>
             <div className= "img-logo" >
-            <img src="https://th.bing.com/th/id/R.6e154f80072e0f134105ec35599d74a6?rik=rPaegHNYFDueCw&pid=ImgRaw&r=0" alt="logo"></img>
-
+                 <img src="https://th.bing.com/th/id/R.6e154f80072e0f134105ec35599d74a6?rik=rPaegHNYFDueCw&pid=ImgRaw&r=0" alt="logo"></img>
             </div>
-            <ul>
-                <li><a href="" ><FiHome/>
+            <ul className='flex'>
+                <li ><FiHome/>
+                <a href="">
                     Home</a></li>
-                <li><a href="" ><FcBriefcase/>
-                    Jobs</a></li>
                 <li><FiBell/>
                     <a href="">Notifications</a></li>
                 <li><FiUser/>
