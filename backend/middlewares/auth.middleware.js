52090b0b2d59e633814c0f2fd20e95797d78d52b
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-10-21 16:09:36 +0300

    backend<>fixing the authmiddleware

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -10,7 +10,7 @@ const authMiddleware = async (req, res, next) => {
     try{
         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
         const user = await User.findOne({email: decoded.email}).lean()
-        req.user = {user};
+        req.user = user;
         next()
 
     }catch(err){
